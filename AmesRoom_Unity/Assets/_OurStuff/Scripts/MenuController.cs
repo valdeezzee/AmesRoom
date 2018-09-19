ce7d977231152648aafc02ff4a311c362a9a19c3
Joe Del Rocco <delrocco@users.noreply.github.com>
2018-09-19 16:17:52 -0400

    Moved our assets into a folder to distinguish from Standard/VR assets.

---
+++ AmesRoom_Unity/Assets/_OurStuff/Scripts/MenuController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuController : MonoBehaviour
+{
+    private SteamVR_TrackedController device;
+
+	void Start()
+    {
+        device = GetComponent<SteamVR_TrackedController>();
+        device.MenuButtonClicked += MenuButtonClicked;
+	}
+
+    private void Update()
+    {
+    }
+
+    private void MenuButtonClicked(object sender, ClickedEventArgs e)
+    {
+        Debug.Log("Menu has been clicked!!!");
+
+        //throw new System.NotImplementedException();
+    }
+}

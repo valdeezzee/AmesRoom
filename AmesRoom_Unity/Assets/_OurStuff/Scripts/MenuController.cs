4af4c918e66dc80a9e837fcc281ab3a28596a62b
Jonathan Valderrama <jvalderrama91@knights.ucf.edu>
2019-10-24 00:48:31 -0400

    Changed scripts
    Clean up some script that i mean to do a while ago. Moved some objects around in the scene.

--- AmesRoom_Unity/Assets/_OurStuff/Scripts/MenuController.cs
+++ AmesRoom_Unity/Assets/_OurStuff/Scripts/MenuController.cs
@@ -1,55 +1,49 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.SceneManagement;
 using Valve.VR;
 using Valve.VR.InteractionSystem;
 
-public class MenuController : MonoBehaviour
+namespace AmesScripts
 {
-    private SteamVR_Controller.Device device;
-    private Hand hand;
-    public bool allowScale = false;
-    public string onlineScene { get; set; }
-    void Start()
+    public class MenuController : MonoBehaviour
     {
-        hand = GetComponent<Hand>();
-    }
-
-    private void Update()
-    {
-        if (hand.controller == null) return;
-        device = SteamVR_Controller.Input((int)hand.controller.index);
-
-        if (hand.controller.GetPressDown(SteamVR_Controller.ButtonMask.ApplicationMenu))
-            MenuButtonClicked();
-        else if (hand.controller.GetPress(SteamVR_Controller.ButtonMask.Trigger) &&
-                hand.controller.GetTouchDown(SteamVR_Controller.ButtonMask.Touchpad) && allowScale)
+        private SteamVR_Controller.Device device;
+        private Hand hand;
+        public bool allowScale = false;
+        public string onlineScene { get; set; }
+        void Start()
         {
-            Vector2 touchPad = (device.GetAxis(EVRButtonId.k_EButton_SteamVR_Touchpad));
+            hand = GetComponent<Hand>();
+        }
 
-            if (touchPad.y > 0.7f)
+        private void Update()
+        {
+            if (hand.controller == null) return;
+            device = SteamVR_Controller.Input((int)hand.controller.index);
+
+            if (hand.controller.GetPressDown(SteamVR_Controller.ButtonMask.ApplicationMenu))
+                MenuButtonClicked();
+            else if (hand.controller.GetPress(SteamVR_Controller.ButtonMask.Trigger) &&
+                    hand.controller.GetTouchDown(SteamVR_Controller.ButtonMask.Touchpad) && allowScale)
             {
-                print("scale up");
-                RoomProperties.instance.SetRoomScale("ScaleUp");
-            }
-            else if (touchPad.y < 0.3f)
-            {
-                print("scale down");
-                RoomProperties.instance.SetRoomScale("ScaleDown");
+                Vector2 touchPad = (device.GetAxis(EVRButtonId.k_EButton_SteamVR_Touchpad));
+
+                if (touchPad.y > 0.7f)
+                {
+                    print("scale up");
+                    RoomProperties.instance.SetRoomScale("ScaleUp");
+                }
+                else if (touchPad.y < 0.3f)
+                {
+                    print("scale down");
+                    RoomProperties.instance.SetRoomScale("ScaleDown");
+                }
             }
         }
 
-
-      
+        private void MenuButtonClicked()
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
     }
-
-    private void MenuButtonClicked()
-    {
-        Debug.Log("Menu has been clicked!!!");
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
-        //throw new System.NotImplementedException();
-    }
-
-    
 }
